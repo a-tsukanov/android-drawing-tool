4093d5e3ebc1beed4a6a0521063c5cec14891397
sasha <sasha.ts91@gmail.com>
2019-01-01 20:38:36 +0300

    camera and gallery ok

--- app/src/main/java/poms/edu/drawingtool/FiguresCollection.kt
+++ app/src/main/java/poms/edu/drawingtool/FiguresCollection.kt
@@ -1,12 +1,12 @@
 package poms.edu.drawingtool
 
-import android.graphics.Color
-
 object FiguresCollection {
-    val figures: MutableList<Figure> = mutableListOf(
-        Circle(100f, 100f, 100f, Color.BLUE),
-        Oval(200f, 500f, 300f, 330f, Color.RED),
-        Rectangle(400f, 500f, 600f, 700f, Color.BLUE),
-        Triangle(800f, 1000f, 1000f, 1200f, Color.BLUE)
-    )
+
+    val figures: MutableList<Figure> = mutableListOf()
+    fun undo() {
+        val size = figures.size
+        if (size == 0) return
+        figures.removeAt(figures.size - 1)
+    }
+
 }
--- app/src/main/java/poms/edu/drawingtool/MainActivity.kt
+++ app/src/main/java/poms/edu/drawingtool/MainActivity.kt
@@ -1,9 +1,16 @@
 package poms.edu.drawingtool
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import yuku.ambilwarna.AmbilWarnaDialog
+
+
 
 class MainActivity : Activity() {
 
@@ -36,5 +43,71 @@ class MainActivity : Activity() {
         btnToFigure.forEach { btn, fig ->
             btn.setOnClickListener { ChosenAttributes.figure = fig }
         }
+
+        btnClear.setOnClickListener {
+            FiguresCollection.figures.clear()
+            drawingView.invalidate()
+        }
+
+        btnUndo.setOnClickListener {
+            FiguresCollection.undo()
+            drawingView.invalidate()
+        }
+
+        btnPhoto.setOnClickListener {
+            val dialog = AlertDialog.Builder(this).apply {
+                setPositiveButton("Gallery") { _, _ ->
+                    val intent = Intent(Intent.ACTION_GET_CONTENT).apply { type = "image/*" }
+                    startActivityForResult(intent, REQUEST_PICK_IMAGE)
+                }
+                setNeutralButton("Camera") { _, _ ->
+                    val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(cameraIntent, REQUEST_CAMERA)
+                }
+            }
+            dialog.show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        fun retrieveImage(): Bitmap {
+            val inputStream = contentResolver.openInputStream(data!!.data!!)
+            return BitmapFactory.decodeStream(inputStream)
+        }
+
+        fun updateWithBitmap(bitmap: Bitmap) {
+
+            val drawable = BitmapDrawable(resources, bitmap)
+            drawingView.background = drawable
+
+            FiguresCollection.figures.clear()
+            drawingView.invalidate()
+        }
+
+
+
+        if (resultCode != Activity.RESULT_OK)
+            return
+
+        when (requestCode) {
+            REQUEST_PICK_IMAGE -> {
+                val bitmap = retrieveImage()
+                updateWithBitmap(bitmap)
+            }
+            REQUEST_CAMERA -> data?.run {
+                val photo = data.extras?.get("data") as Bitmap
+                updateWithBitmap(photo)
+
+            } ?: return
+        }
+
+
+    }
+
+
+    companion object {
+        const val REQUEST_PICK_IMAGE = 1
+        const val REQUEST_CAMERA = 2
     }
 }
